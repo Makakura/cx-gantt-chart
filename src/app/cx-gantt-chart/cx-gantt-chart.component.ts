70cb6d3af4385795cff3207ee0d79623d33a5cca
Makakura <shaharaki@gmail.com>
2020-10-14 15:58:43 +0700

    2 files modified

--- src/app/cx-gantt-chart/cx-gantt-chart.component.ts
+++ src/app/cx-gantt-chart/cx-gantt-chart.component.ts
@@ -1,24 +1,41 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
-import { CxGanttChartModel, CxGanttTaskRenderModel} from './models/gantttask.model';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild
+} from "@angular/core";
+import {
+  CxGanttChartModel,
+  CxGanttTaskRenderModel
+} from "./models/gantttask.model";
 
 @Component({
-  selector: 'cx-gantt-chart',
-  templateUrl: './cx-gantt-chart.component.html',
-  styleUrls: ['./cx-gantt-chart.component.scss']
+  selector: "cx-gantt-chart",
+  templateUrl: "./cx-gantt-chart.component.html",
+  styleUrls: ["./cx-gantt-chart.component.scss"]
 })
 export class CxGanttChartComponent implements OnInit, AfterViewInit {
   @Input() data: CxGanttChartModel;
-  @ViewChild('cxGantChartViewer') cxGantChartViewer: ElementRef<HTMLElement>;
-  @ViewChild('cxGanttChartTasksList') cxGanttChartTasksList: ElementRef<HTMLElement>;
-  @ViewChild('cxGanttChartTimeline', { read: ElementRef }) cxGanttChartTimeline: ElementRef<HTMLElement>;
+  @ViewChild("cxGantChartViewer") cxGantChartViewer: ElementRef<HTMLElement>;
+  @ViewChild("cxGanttChartTasksList") cxGanttChartTasksList: ElementRef<
+    HTMLElement
+  >;
+  @ViewChild("cxGantChartScrollDiv") cxGantChartScrollDiv: ElementRef<
+    HTMLElement
+  >;
+  @ViewChild("cxGanttChartTimeline", { read: ElementRef })
+  cxGanttChartTimeline: ElementRef<HTMLElement>;
 
   taskRenderDataList: CxGanttTaskRenderModel[] = [];
   private yearStep: number = 409; // Width of a step of year, default 409px
+  private currentYear: number = 0;
 
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit() {
+    this.initData();
     this.calculateYearStep();
     this.processTaskDataRender();
   }
@@ -27,24 +44,69 @@ export class CxGanttChartComponent implements OnInit, AfterViewInit {
     this.setHeightForContentViewer();
   }
 
+  onScroll() {
+    const currentScrollLeft = this.cxGantChartScrollDiv.nativeElement
+      .scrollLeft;
+    const currentStep = currentScrollLeft / this.yearStep;
+    const currentStepFloored = Math.floor(currentStep);
+    this.currentYear = this.data.minYear + currentStepFloored;
+    console.log(this.currentYear);
+  }
+
+  onPrev() {
+    this.onChangeYear(this.currentYear - 1);
+  }
+
+  onNext() {
+    this.onChangeYear(this.currentYear + 1);
+  }
+
+  get canPrev(): boolean {
+    return this.currentYear > this.data.minYear;
+  }
+
+  get canNext(): boolean {
+    return this.currentYear > this.data.maxYear;
+  }
+
+  private initData() {
+    this.currentYear = this.data.minYear;
+  }
+
+  private onChangeYear(year: number) {
+    this.currentYear = Math.min(
+      Math.max(year, this.data.minYear),
+      this.data.maxYear
+    );
+    console.log(this.currentYear);
+  }
+
   private setHeightForContentViewer(): void {
-    if (!this.cxGantChartViewer || !this.cxGanttChartTasksList) { return; }
+    if (!this.cxGantChartViewer || !this.cxGanttChartTasksList) {
+      return;
+    }
 
     const cxGantChartViewerElement = this.cxGantChartViewer.nativeElement;
-    const cxGanttChartTasksListElement = this.cxGanttChartTasksList.nativeElement;
+    const cxGanttChartTasksListElement = this.cxGanttChartTasksList
+      .nativeElement;
     const offsetHeight = 100; // px
-    const contentHeight = cxGanttChartTasksListElement.offsetHeight + offsetHeight;
-  
-    if (contentHeight > 0 ) {
-      cxGantChartViewerElement.style.height = contentHeight + 'px';
+    const contentHeight =
+      cxGanttChartTasksListElement.offsetHeight + offsetHeight;
+
+    if (contentHeight > 0) {
+      cxGantChartViewerElement.style.height = contentHeight + "px";
     }
   }
 
   private calculateYearStep() {
-    if (!this.cxGanttChartTimeline) { return; }
+    if (!this.cxGanttChartTimeline) {
+      return;
+    }
 
     const cxGanttChartTimelineElement = this.cxGanttChartTimeline.nativeElement;
-    const firstYearElement = cxGanttChartTimelineElement.querySelector('.cx-gantt-timeline-year-container');
+    const firstYearElement = cxGanttChartTimelineElement.querySelector(
+      ".cx-gantt-timeline-year-container"
+    );
     if (firstYearElement) {
       this.yearStep = firstYearElement.clientWidth;
     }
@@ -52,11 +114,13 @@ export class CxGanttChartComponent implements OnInit, AfterViewInit {
 
   private processTaskDataRender(): void {
     if (!this.data || !this.data.tasks || this.data.tasks.length <= 0) {
-      console.error('Invalid data input for cx-gantt-chart');
+      console.error("Invalid data input for cx-gantt-chart");
       return;
     }
-    
-    this.taskRenderDataList = this.data.tasks.map(task => new CxGanttTaskRenderModel(task, this.data.minYear, this.yearStep));
+
+    this.taskRenderDataList = this.data.tasks.map(
+      task => new CxGanttTaskRenderModel(task, this.data.minYear, this.yearStep)
+    );
     console.log(this.taskRenderDataList);
   }
 }
